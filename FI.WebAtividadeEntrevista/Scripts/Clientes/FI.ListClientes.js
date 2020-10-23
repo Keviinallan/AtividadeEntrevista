1352fff5a831905039971712d511f8f8ba56233a
Kevin Allan <kevinallan_@hotmail.com>
2020-10-23 11:10:59 -0300

    Atividade da entrevista

---
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/FI.ListClientes.js
@@ -0,0 +1,51 @@
+﻿
+$(document).ready(function () {
+
+    if (document.getElementById("gridClientes"))
+        $('#gridClientes').jtable({
+            title: 'Clientes',
+            paging: true,
+            pageSize: 5,
+            sorting: true,
+            defaultSorting: 'Nome ASC',
+           
+            actions: {
+                listAction: urlClienteList,
+            },
+            
+            fields: {
+                CPF: {
+                    title: 'CPF',
+                    width: 'auto'
+                },
+                Nome: {
+                    title: 'Nome',
+                    width: 'auto'
+                },
+                Sobrenome: {
+                    title: 'Sobrenome',
+                    width: 'auto'
+                },
+                Telefone: {
+                    title: 'Telefone',
+                    width: 'auto'
+                },
+                Email: {
+                    title: 'Email',
+                    width: 'auto'
+                },
+                Alterar: {
+                    title: '',
+                    display: function (data) {
+                        return '<div style="text-align: center"> <button onclick="window.location.href=\'' + urlAlteracao + '/' + data.record.Id + '\'" class="btn btn-primary btn-sm">Alterar</button></div>';
+                    }
+                }
+            }
+        });
+
+
+    if (document.getElementById("gridClientes"))
+        $('#gridClientes').jtable('load');
+})
+
+
